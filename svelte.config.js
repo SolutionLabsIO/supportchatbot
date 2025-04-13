a799aa6dcc36b2331a5fc41f3e370e29706e47b6
Charles <charles@solutionlabs.io>
2025-04-13 18:08:25 -0400

    Fixing nodejs

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,13 @@
-import adapter from '@sveltejs/adapter-auto';
+import vercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: { 
+		adapter: vercel({
+			runtime: 'nodejs20.x'
+		})
+	}
 };
 
 export default config;
